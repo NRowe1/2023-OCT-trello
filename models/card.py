43d7cc82bbce3e35d5a5470da0df974c7ae20833
Nicholas Rowe <148307107+NRowe1@users.noreply.github.com>
2024-02-15 19:40:18 +1000

    Add cards data for seeding the cards table

--- models/card.py
+++ models/card.py
@@ -1,18 +1,29 @@
-from init import db, ma 
+from init import db, ma
 from marshmallow import fields
+
 class Card(db.Model):
     __tablename__ = "cards"
 
-    id - db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     description = db.Column(db.Text)
-    date = db.Column(db.Date)
+    date = db.Column(db.Date) # Date the card was created
     status = db.Column(db.String)
     priority = db.Column(db.String)
 
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
     user = db.relationship('User', back_populates='cards')
+
+    # {id: 1, title: Card 1, user_id: 2}
+    # {
+    #   id: 1,
+    #   title: Card 1,
+    #   user: {
+    #       name: User 1,
+    #       email: user1@email.com
+    #   }
+    # }
 
 class CardSchema(ma.Schema):
 
